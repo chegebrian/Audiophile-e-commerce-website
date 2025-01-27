75e691df492aa1042dec0840c879c03c880f6026
Brian Waithaka <waithakabrian8@gmail.com>
2025-01-27 21:08:07 +0300

    implement a responsive header component

--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -1,13 +1,24 @@
-import React, { useState } from "react";
+import { useState } from "react";
 import { NavLink } from "react-router";
 
 function Header() {
+  const [isOpen, setIsopen] = useState(false);
+
+  function setOpen() {
+    setIsopen((isOpen) => !isOpen);
+  }
   return (
     <header className="flex items-center justify-between bg-black">
-      <span className="cursor-pointer text-3xl font-extrabold text-white">
+      <img
+        src={`${isOpen ? "/images/shared/tablet/icon-close-menu.svg" : "/images/shared/tablet/icon-hamburger.svg"}`}
+        alt="open icon"
+        className="cursor-pointer lg:hidden"
+        onClick={setOpen}
+      />
+      <span className="cursor-pointer text-3xl font-extrabold text-white md:max-lg:ml-10">
         audiophile
       </span>
-      <nav className="flex items-center gap-6">
+      <nav className={`items-center gap-6 lg:flex ${isOpen ? "" : "hidden"}`}>
         <NavLink
           to="/"
           className="text-sm font-bold tracking-widest text-white uppercase transition duration-300 hover:text-amber-600"
@@ -33,7 +44,11 @@ function Header() {
           earphones
         </NavLink>
       </nav>
-      <img src="/images/shared/desktop/icon-cart.svg" alt="cart" />
+      <img
+        src="/images/shared/desktop/icon-cart.svg"
+        alt="cart"
+        className="cursor-pointer md:max-lg:ml-auto"
+      />
     </header>
   );
 }
